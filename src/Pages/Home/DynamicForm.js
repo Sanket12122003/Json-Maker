e3ebb801656ba15c964c2c53bd035645cf6cc08a
Sanket12122003 <sanketpawarsp1212@gmail.com>
2024-01-20 09:35:08 +0530

    Dynamic form creation

--- src/Pages/Home/DynamicForm.js
+++ src/Pages/Home/DynamicForm.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import InputField from "../../components/InputField";
-import GroupField from "../../components/GroupField";
-import { Box, Button, VStack } from "@chakra-ui/react";
 import FormComponent from "../../components/FormComponent";
 import SwitchField from "../../components/SwitchField";
 import SelectField from "../../components/SelectField";
 import RadioField from "../../components/RadioField";
+import {
+  Box,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Text,
+} from "@chakra-ui/react";
 
-const arr = [];
 
-
+export const FormDataContext = React.createContext();
 
 const DynamicForm = ({ formSchema }) => {
   const [formData, setFormData] = useState({});
@@ -21,62 +29,97 @@ const DynamicForm = ({ formSchema }) => {
     }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(formData);
-    // Send formData to the backend as needed
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const renderJsonElements = (schema) => {
+    return (
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>JSON to be sent to the backend</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box>
+            {Object.entries(schema).map(([key, value]) => (
+                <Text key={key} marginTop={2}>
+                  <strong>{key}: </strong>
+                  {JSON.stringify(value, null, 2)}
+                </Text>
+              ))}
+
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    );
   };
 
   return (
     <>
-      <form>
-        {formSchema.map((schema, index) => {
-          return (
-            <React.Fragment key={index}>
-              {schema.uiType === "Input" && (
-                <InputField
-                  schema={schema}
-                  key={schema.sort}
-                  updateFormData={updateFormData}
-                />
-              )}
+          <FormDataContext.Provider value={updateFormData}>
+        <form>
+          {formSchema.map((schema, index) => {
+            return (
+              <React.Fragment key={index}>
+                {schema.uiType === "Input" && (
+                  <InputField
+                    schema={schema}
+                    key={schema.sort}
+                    updateFormData={updateFormData}
+                  />
+                )}
 
-              {schema.uiType === "Switch" && (
-                <SwitchField
-                  schema={schema}
-                  key={schema.sort}
-                  updateFormData={updateFormData}
-                />
-              )}
+                {schema.uiType === "Switch" && (
+                  <SwitchField
+                    schema={schema}
+                    key={schema.sort}
+                    updateFormData={updateFormData}
+                  />
+                )}
 
-              {schema.uiType === "Select" && (
-                <SelectField
-                  schema={schema}
-                  key={schema.sort}
-                  updateFormData={updateFormData}
-                />
-              )}
+                {schema.uiType === "Select" && (
+                  <SelectField
+                    schema={schema}
+                    key={schema.sort}
+                    updateFormData={updateFormData}
+                  />
+                )}
 
-              {schema.uiType === "Radio" && (
-                <RadioField
-                  schema={schema}
-                  key={schema.sort}
-                  updateFormData={updateFormData}
-                />
-              )}
 
-              {schema.uiType === "Group" && (
-                <FormComponent
-                  schema={schema}
-                  key={schema.sort}
-                  updateFormData={updateFormData}
-                />
-              )}
-            </React.Fragment>
-          );
-        })}
-        <Button onClick={handleSubmit}>Submit</Button>
-      </form>
+                {schema.uiType === "Radio" && (
+                  <RadioField
+                    schema={schema}
+                    key={schema.sort}
+                    updateFormData={updateFormData}
+                  />
+                )}
+
+{schema.uiType === "Group" && (
+                  <FormComponent
+                    schema={schema}
+                    key={schema.sort}
+                    updateFormData={updateFormData}
+                  />
+                )}
+              </React.Fragment>
+            );
+          })}
+          {formSchema.length > 0 && (
+            <Box style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+              <Button Button colorScheme='blue' marginTop={"10px"} onClick={handleOpenModal} >Submit</Button>
+              {renderJsonElements(formData)}
+            </Box>
+          )}
+        </form>
+      </FormDataContext.Provider>
     </>
   );
 };
